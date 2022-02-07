44f81cac0ce6e66b281d22e039e6af440feaa899
Andres Diaz <andiazo@unal.edu.co>
2022-02-07 15:34:00 -0500

    None

--- src/ast.py
+++ src/ast.py
@@ -66,12 +66,84 @@ def dotop(variable):
         i = variable[1]
         j = variable[2]
         k = variable[3]
+        rachas, matrixrachas = multicot(matrix)
+        print("-----", matrixrachas)
+        dotoprachas_result = dotoprachas(matrixrachas, 0,0,0)
+        print("----------------", dotoprachas_result)
         lenmatrix = len(matrix)
         for i in range(lenmatrix):
             l = len(matrix[i])
             print(l)
         
     return 0
+
+
+def dotoprachas(matrixrachas, i, j, k):
+    print("matrixrachas", matrixrachas)
+    sum_rachas_total = sum(sum(sum(matrixrachas,[]),[]))
+    print("as",sum(sum(sum(matrixrachas,[]),[])))
+    print("a1",sum(sum(matrixrachas[1],[])))
+    print("a2",sum(sum(matrixrachas[:][1],[])))
+    print("a3",sum(sum(matrixrachas[1][1],[])))
+    if i == j == k == 0:
+        pass
+    elif i == j == k > 0:
+        rachas_ijk = matrixrachas[i][j][k]
+    else:
+        if i > 0 and (j == k == 0):
+            return sum(sum(matrixrachas[i-11],[]))
+        elif j > 0 and (i == k == 0):
+            return sum(sum(matrixrachas[:][j-1],[]))
+        elif i == j > 0 and k == 0:
+            return sum(sum(matrixrachas[i-1][j-1],[]))
+        else:
+            raise KeyError
+
+def multicot(matrix):
+    dim1 = len(matrix)
+    dims = [len(matrix[i]) for i in range(dim1)]
+    print("dims", dims)
+    dims2 = []
+    for i in range(dim1):
+        if i == 0:
+            dims2.append(dims[i])
+        else:
+            dims2.append(dims[i]+dims2[i-1])
+        print(i)
+
+    print("dims2", dims2)
+    union = sum(matrix, [])
+    union.sort()
+    mc = funmulticot(union)
+    rachas = [1 for i in range(len(mc))]
+    if (len(mc) != 0):
+        prev = mc[0]
+        rachas[0] = 1
+        for i in range(1,len(mc)):
+            if mc[i] == prev:
+                rachas[i] = rachas[i-1]
+            else:
+                rachas[i] += rachas[i-1]
+            prev = mc[i]
+
+    matriznumrachas = []
+    for i in range(dim1):
+        if i == 0:
+            lista = [rachas[:dims2[i]]]
+        else:
+            lista = [rachas[dims2[i-1]:dims2[i]]]
+        matriznumrachas.append(lista)
+
+    return rachas, matriznumrachas
+
+def funmulticot(vector):
+    mc = []
+    for i in range(len(vector)):
+        if vector[i] > 0:
+            mc.append(1)
+        else:
+            mc.append(0)
+    return mc
 
 functions = {
 	'range': range_of,
